9f40b67d235547ee516caca026f910cfe4d8f123
RC <187284857+RetiredC@users.noreply.github.com>
2025-01-01 16:01:44 +0400

    Add files via upload

--- ExpKangDlg.cpp
+++ ExpKangDlg.cpp
@@ -28,8 +28,8 @@
 //Proposed here advanced loop handling does not break a chain of jumps and reduces the number of large loops for high ranges so K is the same as it must be.
 //the difference between Simple and Advanced methods grows with growing RANGE_BITS and DP_BITS
 
-#define RANGE_BITS			(40 + 8)
-#define DP_BITS				(10)
+#define RANGE_BITS			(40 + 0)
+#define DP_BITS				(8)
 
 //Usually, the higher the range, the more number of kangaroos is used (because more devices are used for solving), so their paths grow a bit slower than sqrt(range) and it helps to keep K stable.
 //but you can use fixed number of kangaroos to confirm that everything works properly for any range even without increasing the number of kangaroos
@@ -59,10 +59,10 @@
 // MD_LEN 16 and MD_CNT 10 mean we can handle loops up to 2^40
 
 //if defined, we stop searching a key after specified number of iterations so we can check loop stats for high RANGE_BITS
- #define SYNTHETIC_TEST
+// #define SYNTHETIC_TEST
 #ifdef SYNTHETIC_TEST
 	#define MAX_TOTAL_ITERS		(100 * 1000 * 1000ull)
-	#define RANGE_BITS			74
+	#define RANGE_BITS			60
 	#define POINTS_CNT			CPU_THR_CNT
 	#define KANG_CNT			1
 	#define ESCAPE_FROM_LARGE_LOOPS
